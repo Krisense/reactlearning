4dcf2e3c6799209142a69adf9a764aa9f289b4b1
Krisense <kristian.kristian.2014@mail.ru>
2025-04-14 15:07:59 +0300

    Отображение отсутствия постов
    Отсутствие постов теперь в PostList

--- src/App.js
+++ src/App.js
@@ -16,24 +16,22 @@ function App() {
     { id: 3, title: "cc 3", body: "a" },
   ]);
 
+  const [filter, setFilter] = useState({ sort: "", query: "" });
 
-
-
-  const[filter, setFilter] = useState({sort:'', query:''})
-
- 
-
-  const sortedPosts = useMemo(()=>{
-   
-    if(filter.sort){
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
     }
     return posts;
-  }, [filter.sort, posts])
+  }, [filter.sort, posts]);
 
-  const sortedAndSearchedPosts = useMemo(()=>{
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(filter.query)
+    );
+  }, [filter.query, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -42,18 +40,18 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
-  
-  
 
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <hr style = {{margin: '15px 0'}}/>
-      <PostFilter filter ={filter} setFilter={setFilter}/>
-      {sortedAndSearchedPosts.length 
-        ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов JS" />
-        : <h1 style ={{textAlign:'center'}}>Посты не найдены!</h1>
-      }
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="Список постов JS"
+      />
     </div>
   );
 }
