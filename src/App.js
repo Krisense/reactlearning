015dcd3f9b25a97158bb26bde49951ff7c0d0964
Krisense <kristian.kristian.2014@mail.ru>
2025-04-15 17:33:28 +0300

    Анимации удаления/добавления постов
    Добавил анимации удаления и добавления, импортировал и установил TransitionGroup. В PostList использовал createRef. Также создал кастомный хук usePosts, в котором будет происходить сортировка и фильтрация постов. Теперь App.js более опрятный и читаемый.

--- src/App.js
+++ src/App.js
@@ -9,32 +9,17 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "aa", body: "z" },
-    { id: 2, title: "bb 2", body: "v" },
-    { id: 3, title: "cc 3", body: "a" },
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+ 
 
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React,{useRef} from "react";
 import PostItem from "./PostItem";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 
 const PostList = ({ posts, title, remove }) => {
-  if (!posts.length){
-    return(
-    <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>
-
-  )}
-  return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>{title}</h1>
-      {posts.map((post, index) => (
-        <PostItem
-          remove={remove}
-          number={index + 1}
-          post={post}
-          key={post.id}
-        />
-      ))}
-    </div>
-  );
+    const nodeRefs = useRef({});
+    if (!posts.length) {
+        return (
+            <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
+        );
+    }
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>{title}</h1>
+            <TransitionGroup>
+                {posts.map((post, index) =>{
+                if(!nodeRefs.current[post.id]){
+                    nodeRefs.current[post.id] = React.createRef();
+                }
+                return(
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                        nodeRef={nodeRefs.current[post.id]}
+                    >   
+                    <div ref={nodeRefs.current[post.id]}>
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                        </div>
+                    </CSSTransition>
+                    );
+                })}
+            </TransitionGroup>
+        </div>
+    );
 };
 
 export default PostList;
