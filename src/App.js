7766e2ebbe1074acfba9f769af1d144525a8379f
Krisense <kristian.kristian.2014@mail.ru>
2025-04-16 21:22:03 +0300

    Кастомный хук UseFetching
    Добавил кастомный хук, принимающий колбэк и возвращающий массив fetching, isLoading, error. Этот массив мы используем в App, используя хук.

--- src/App.js
+++ src/App.js
@@ -13,13 +13,18 @@ import { usePosts } from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -29,16 +34,6 @@ function App() {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async ()=>{
-      const posts = await PostService.getAll();
-    setPosts(posts);
-    setIsPostsLoading(false);
-    }, 1000)
-    
-  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -54,10 +49,21 @@ function App() {
       </MyModal>
 
       <hr style={{ margin: "15px 0" }} />
+
       <PostFilter filter={filter} setFilter={setFilter} />
 
+      {postError && <h1>Произошла ошибка ${postError}</h1>}
+
       {isPostsLoading ? (
-        <div style ={{display:"flex", marginTop:"50px", justifyContent:"center"}}><Loader/></div>
+        <div
+          style={{
+            display: "flex",
+            marginTop: "50px",
+            justifyContent: "center",
+          }}
+        >
+          <Loader />
+        </div>
       ) : (
         <PostList
           remove={removePost}
