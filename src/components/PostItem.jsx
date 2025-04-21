256ae6f4024a36b38955bc41c2dc567acf2c72bc
Krisense <kristian.kristian.2014@mail.ru>
2025-04-21 20:08:58 +0300

    Открытие постов, загрузка комментариев
    Добавил postIdPage для каждого поста(динамическая навигация), переход через кнопку в postItem. Хук useNavigate для навигации. В postIdPage хук useParams для сбора параметров из URL, загрузка комментариев с помощью кастомного useFetching.

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import MyButton from './UI/button/MyButton';
+import React from "react";
+import MyButton from "./UI/button/MyButton";
+import {useNavigate} from 'react-router-dom'
 
 const PostItem = (props) => {
-     
-    return (
-        <div className="post">
-        <div className="post__content">
-            <strong>{props.post.id} {props.post.title}</strong>
-            <div>
-            {props.post.body}
-            </div>
-        </div>
-        <div className="post__btns">
-          <MyButton onClick ={() => props.remove(props.post)}>
-            Delete</MyButton>
-        </div>
-    </div> 
-    );
+    const router = useNavigate()
+    console.log(router)
+    function transitToPost(id) {
+        router(`/posts/${id}`, { replace: true })
+      }
+  return (
+    <div className="post">
+      <div className="post__content">
+        <strong>
+          {props.post.id} {props.post.title}
+        </strong>
+        <div>{props.post.body}</div>
+      </div>
+      <div className="post__btns">
+        <MyButton onClick={() => transitToPost(props.post.id)}>Открыть</MyButton>
+        <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
+      </div>
+    </div>
+  );
 };
 
 export default PostItem;
